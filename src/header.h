62933832bf184156506051bfa138871bb4d17395
Andrei <u.andrei678@gmail.com>
2023-03-05 15:01:56 -0500

    Initial commit.

---
+++ src/header.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+
+class Image {
+private:
+	struct Header {
+		char idLen;
+		char hasColorMap;
+		char imageType;
+		short colorMapOrigin;
+		short colorMapLen;
+		short colorMapDepth;
+		short xOrigin;
+		short yOrigin;
+		short width;
+		short height;
+		char bitsPerPixel;
+		char imageDescriptor;
+
+		Header(char id, char colorMap, char imageType, short colorOrigin, short colorLen, short colorDepth, short x, short y, short width, short height, char bits, char descriptor) {
+			this->idLen = id;
+			this->hasColorMap = colorMap;
+			this->imageType = imageType;
+			this->colorMapOrigin = colorOrigin;
+			this->colorMapDepth = colorDepth;
+			this->xOrigin = x;
+			this->yOrigin = y;
+			this->width = width;
+			this->height = height;
+			this->bitsPerPixel = bits;
+			this->imageDescriptor = descriptor;
+		}
+	};
+public:
+	Image();
+};
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void LoadFile(); // loads the .tga file
+
+int main() {
+
+	return 0;
+}
